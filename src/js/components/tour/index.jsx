c38b1bcad237eaf2ca6f868fe306f2489a2f313a
RuubenL <ruuben.l@live.be>
2017-06-09 21:30:50 +0200

    :sparkles: added tours page

--- src/js/components/tour/index.jsx
+++ src/js/components/tour/index.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
+import {string, number} from 'prop-types';
 
-const Tour = () => {
+import {Link} from 'react-router-dom';
+import {observer} from 'mobx-react';
+
+const Tour = ({tour, tourTitle, tourText, _id}) => {
   return (
     <li>
-      <img></img>
+      <img src={`../../../assets/svg/${tour}.svg`} alt={`${tour}`} />
       <header>
-        <h2>Tour</h2>
+        <h2>{tourTitle}</h2>
       </header>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p>{tourText}</p>
+
+      <Link to={`tours/${_id}`} >
+        <p>Ontdek meer</p>
+      </Link>
+
     </li>
   );
 };
 
-export default Tour;
+Tour.propTypes = {
+  tour: string.isRequired,
+  tourTitle: string.isRequired,
+  tourText: string.isRequired,
+  _id: number.isRequired
+};
+
+export default observer(Tour);
